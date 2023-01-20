a3657b82ce701d66ab591f2081e58dc0e7d9a399
emilybaileyANDdigital <emily.bailey@and.digital>
2023-01-20 15:47:17 +0000

    text set up for tooltips

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,46 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view />
+    <b-card title="Hello world!"></b-card>
+    <b-card-text>Spikes and lots of them</b-card-text>
+    <b-card title="Tooltiptesting">
+      <b-card-text class="p-3"
+        >I'm baby do brunch la croix chicharrones sunt. Gatekeep lorem keffiyeh
+        everyday carry celiac, fit organic dolor. Paleo +1 lorem hashtag.
+        Scenester food truck artisan godard fit pop-up exercitation vinyl tilde.
+        DSA chambray veniam, big mood irure keytar snackwave kale chips coloring
+        book enim quis semiotics whatever plaid culpa. Food truck id ad banjo
+        commodo meditation. Brunch artisan iPhone hashtag aliquip in shabby chic
+        listicle tonx franzen quis veniam. Jianbing craft beer seitan you
+        probably haven't heard of them tacos ethical quinoa subway tile vice
+        cillum. Echo park keffiyeh vexillologist, sus cornhole adipisicing you
+        probably haven't heard of them church-key anim nulla meditation
+        vaporware ea. Chartreuse deserunt echo park, hashtag typewriter officia
+        scenester austin kinfolk etsy pabst paleo non aute seitan. Hexagon viral
+        lumbersexual photo booth bitters VHS gatekeep pork belly consectetur la
+        croix anim. Vibecession semiotics polaroid jianbing kombucha stumptown.
+        Vice jean shorts enim lo-fi godard synth mixtape laboris laborum yuccie
+        pinterest tote bag migas selfies. Ugh selfies distillery raw denim.
+        Dummy text? More like dummy thicc text, amirite?</b-card-text
+      >
+      <b-card-text class="p-3"
+        >Cray tofu est shoreditch in vegan cupidatat cardigan sriracha
+        kickstarter officia fingerstache humblebrag adipisicing. Distillery in
+        twee sed beard. Ramps lorem man braid scenester eu. Consequat photo
+        booth pitchfork crucifix sed food truck. Eu street art reprehenderit,
+        waistcoat humblebrag chia small batch swag salvia schlitz. Selvage VHS
+        semiotics vinyl, lorem coloring book tousled chicharrones YOLO PBR&B
+        edison bulb mlkshk slow-carb hoodie copper mug. Tofu retro lumbersexual,
+        sriracha JOMO locavore schlitz poke pitchfork photo booth. Sartorial
+        irony asymmetrical fixie authentic, jean shorts chicharrones VHS ut.
+        Sartorial esse tbh microdosing, waistcoat neutra hashtag retro shaman do
+        kogi taxidermy. Occaecat shabby chic crucifix, tonx edison bulb freegan
+        asymmetrical ascot gastropub lomo swag do vegan in deserunt. You
+        probably haven't heard of them pickled chambray, cray quis fashion axe
+        nostrud next level. Asymmetrical umami offal dolor gochujang leggings
+        trust fund blog aesthetic live-edge YOLO. Hashtag post-ironic
+        farm-to-table pitchfork.</b-card-text
+      >
+    </b-card>
   </div>
 </template>
 
